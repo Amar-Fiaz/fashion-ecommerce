06e00bcb5b090dd31c33d5cee33fd7aba45ae061
Amar-Fiaz <ikamar588@gmail.com>
2026-09-02 16:59:49 +0500

    feat: add cart and wishlist routes

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -10,6 +10,8 @@ import ForgotPasswordPage from "../pages/ForgotPasswordPage";
 import ResetPasswordPage from "../pages/ResetPasswordPage";
 import VerifyEmailPage from "../pages/VerifyEmailPage";
 import ProfilePage from "../pages/ProfilePage";
+import CartPage from "../pages/CartPage";
+import WishlistPage from "../pages/WishlistPage";
 import AdminLoginPage from "../pages/AdminLoginPage";
 import AdminPlaceholderPage from "../pages/AdminPlaceholderPage";
 
@@ -25,9 +27,11 @@ function AppRoutes() {
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
         <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
         <Route path="/verify-email/:token" element={<VerifyEmailPage />} />
+        <Route path="/cart" element={<CartPage />} />
 
         <Route element={<ProtectedRoute />}>
           <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/wishlist" element={<WishlistPage />} />
         </Route>
       </Route>
 
